4579b7ec01c3522f50ee330be6d88663ecc1fe1a
Michael Lowe <michael.lowe@education.govt.nz>
2023-03-20 23:45:20 +1300

    Add subscription creating code

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,19 @@ import {
 } from "aws-cdk-lib/aws-iam";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Topic } from "aws-cdk-lib/aws-sns";
+import { SmsSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
 import { Construct } from "constructs";
+import { readFileSync } from "fs";
+import { load } from "js-yaml";
 
 export class LeagueLobsterTextReminder extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    const handler = new NodejsFunction(this, "GetGamesFunction", {
+    new NodejsFunction(this, "GetGamesFunction", {
       runtime: Runtime.NODEJS_18_X,
     });
-
-    console.log(handler);
 
     const githubProvider = new OpenIdConnectProvider(
       this,
@@ -30,7 +32,7 @@ export class LeagueLobsterTextReminder extends Stack {
       }
     );
 
-    const deployRole = new Role(this, "GithubDeployRole", {
+    new Role(this, "GithubDeployRole", {
       assumedBy: new WebIdentityPrincipal(
         githubProvider.openIdConnectProviderArn,
         {
@@ -56,7 +58,16 @@ export class LeagueLobsterTextReminder extends Stack {
       },
     });
 
-    console.log(deployRole);
+    const data = readFileSync("./src/contacts.yml", "utf-8");
+    const contacts: any = load(data);
+    contacts.teams.forEach((team: any) => {
+      console.log("TEAM:", team.Name);
+      const teamTopic = new Topic(this, `${team.Name}Alerts`);
+      team.Players.forEach((player: any) => {
+        console.log("PLAYER:", player);
+        teamTopic.addSubscription(new SmsSubscription(player.Number));
+      });
+    });
   }
 }
 
