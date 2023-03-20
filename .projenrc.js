941d9732df179f535d79ead57ce93b49c4f6dbfe
Michael Lowe <michael.lowe@education.govt.nz>
2023-03-20 22:41:40 +1300

    Change projen settings for no ESlint

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'league-lobster-text-reminders',
-
+  eslint: false,
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
