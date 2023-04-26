89c050e0c146d39b031307a8a44de76aa99d9353
Michael Lowe <michael.lowe@education.govt.nz>
2023-04-26 16:47:12 +1200

    Try environment handling differently

--- src/main.ts
+++ src/main.ts
@@ -23,40 +23,26 @@ const prodEnv = {
 
 const app = new App();
 
-/**
- * This is what happens when you run a local synth without specifying a NODE_ENV
- */
-
-/**
- * If we are in dev, we can use the local contacts.yml file, but in our pipelines,
- * we grab this from the environment variables.
- */
-
-// If there is a contacts.yml (in dev, then just use that for the contacts)
-
-// If we are deploying to a test or staging environment, use the test contacts from the contacts.ts file
-
-// If we are deploying to production, then we use the environment var
 let localContacts;
+let prodContacts: any;
 
 if (existsSync("./src/contacts.yml")) {
   localContacts = readFileSync("./src/contacts.yml", "utf-8");
 }
 
-// } else if (process.env.CI === "true" && process.env.NODE_ENV !== "prod") {
-//   data = readFileSync("./src/test.contacts.yml", "utf-8");
-// } else {
-//   data = process.env.CONTACTS_YML;
-// }
+if (process.env.CONTACTS_YML) {
+  prodContacts = load(process.env.CONTACTS_YML);
+} else {
+  prodContacts = Contacts.Test;
+}
 
-const contacts: any = load(localContacts ? localContacts : "");
+const localTeams: any = load(localContacts ? localContacts : "");
 
 new LeagueLobsterTextReminder(app, "league-lobster-text-reminders-dev", {
   env: devEnv,
-  Contacts: contacts,
+  Contacts: localTeams,
 });
 
-new OIDCSetup(app, "oidc-setup", { env: testEnv });
 new LeagueLobsterTextReminder(app, "league-lobster-text-reminders-test", {
   env: testEnv,
   Contacts: Contacts.Test,
@@ -64,7 +50,9 @@ new LeagueLobsterTextReminder(app, "league-lobster-text-reminders-test", {
 
 new LeagueLobsterTextReminder(app, "league-lobster-text-reminders-prod", {
   env: prodEnv,
-  Contacts: contacts,
+  Contacts: prodContacts,
 });
+
+new OIDCSetup(app, "oidc-setup", { env: testEnv });
 
 app.synth();
