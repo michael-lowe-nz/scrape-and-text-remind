8e453dff982a6e71626e8c1d24580322a961a6fe
Michael Lowe <michael.lowe@education.govt.nz>
2023-05-02 11:52:08 +1200

    Refactor to use stages

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,76 @@
-import { App } from "aws-cdk-lib";
+import { App, Stage, StageProps } from "aws-cdk-lib";
 import { OIDCSetup } from "./lib/stacks/oidcSetup";
 import { LeagueLobsterTextReminder } from "./lib/stacks/leagueLobsterTextReminders";
-import { existsSync, readFileSync } from "fs";
+// import { existsSync, readFileSync } from "fs";
 import { load } from "js-yaml";
-import Contacts from "./contacts";
+import ContactData from "./contacts";
+import { Contacts } from "./types";
+import { Construct } from "constructs";
+// import { Construct } from "constructs";
 
 // for development, use account/region from cdk cli
-const testEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
+// const testEnv = {
+//   account: process.env.CDK_DEFAULT_ACCOUNT,
+//   region: process.env.CDK_DEFAULT_REGION,
+// };
 
 const devEnv = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEFAULT_REGION,
 };
 
-const prodEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
+// const prodEnv = {
+//   account: process.env.CDK_DEFAULT_ACCOUNT,
+//   region: process.env.CDK_DEFAULT_REGION,
+// };
 
 const app = new App();
 
-let localContacts: any;
+// let localContacts: any;
 let prodContacts: any;
 
-if (existsSync("./src/contacts.yml")) {
-  const localContactsYml = readFileSync("./src/contacts.yml", "utf-8");
-  localContacts = load(localContactsYml);
-} else {
-  localContacts = Contacts.Test;
-}
+// if (existsSync("./src/contacts.yml")) {
+//   const localContactsYml = readFileSync("./src/contacts.yml", "utf-8");
+//   localContacts = load(localContactsYml);
+// } else {
+//   localContacts = Contacts.Test;
+// }
 
 if (process.env.CONTACTS_YML) {
   prodContacts = load(process.env.CONTACTS_YML);
 } else {
-  prodContacts = Contacts.Test;
+  prodContacts = ContactData.Test;
 }
 
-new LeagueLobsterTextReminder(app, "league-lobster-text-reminders-dev", {
+new OIDCSetup(app, "oidc-setup", { env: devEnv });
+
+interface StageTemplateProps extends StageProps {
+  contacts: Contacts;
+}
+
+class TextRemindersStage extends Stage {
+  constructor(scope: Construct, id: string, props: StageTemplateProps) {
+    super(scope, id, props);
+
+    new LeagueLobsterTextReminder(this, "TextReminders", {
+      Contacts: props.contacts,
+    });
+  }
+}
+
+new TextRemindersStage(app, "dev-stage", {
   env: devEnv,
-  Contacts: localContacts,
+  contacts: ContactData.Test,
 });
 
-new LeagueLobsterTextReminder(app, "league-lobster-text-reminders-test", {
-  env: testEnv,
-  Contacts: Contacts.Test,
+new TextRemindersStage(app, "test-stage", {
+  env: devEnv,
+  contacts: ContactData.Test,
 });
 
-new LeagueLobsterTextReminder(app, "league-lobster-text-reminders-prod", {
-  env: prodEnv,
-  Contacts: prodContacts,
+new TextRemindersStage(app, "prod-stage", {
+  env: devEnv,
+  contacts: prodContacts,
 });
-
-new OIDCSetup(app, "oidc-setup", { env: testEnv });
 
 app.synth();
