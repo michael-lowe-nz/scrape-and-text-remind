0712e384bac7ea54e9ba812e233cc6158170fa6a
Michael Lowe <michael.lowe@education.govt.nz>
2023-05-10 01:09:18 +1200

    Comment out for now

--- src/main.GetGamesFunction.ts
+++ src/main.GetGamesFunction.ts
@@ -1,13 +1,13 @@
 import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
-import axios from "axios";
+// import axios from "axios";
 import { getRandomEmoji } from "./lib/getRandomEmoji";
 
 exports.handler = async () => {
   const client = new SNSClient({ region: process.env.AWS_REGION });
   const date = new Date();
-  const url = `https://websites.mygameday.app/team_info.cgi?c=0-2854-0-622183-27083591&a=SFIX`;
-  const data = await axios.get(url);
-  console.log(data);
+  // const url = `https://websites.mygameday.app/team_info.cgi?c=0-2854-0-622183-27083591&a=SFIX`;
+  // const data = await axios.get(url);
+  // console.log(data);
 
   const Message = `Gen-X Reminders: Game vs. ${date.toString()}${getRandomEmoji()} `;
   const params = {
@@ -20,4 +20,4 @@ exports.handler = async () => {
     statusCode: 200,
     body: JSON.stringify(response),
   };
-};git st
+};
