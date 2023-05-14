2b8890eab604d13d41ee585506b6f8f099fc2dd0
Michael Lowe <michael.lowe@education.govt.nz>
2023-05-15 00:00:36 +1200

    Update tests for getGameFunction

--- src/lib/getThisWeeksGameFromHtml.ts
+++ src/lib/getThisWeeksGameFromHtml.ts
@@ -1,61 +1,33 @@
 import cheerio from "cheerio";
 import moment from "moment";
+import { Game } from "../types";
 
 const columnMapping: any = {
-  1: "Date",
-  2: "Time",
-  3: "Court",
-  6: "TeamAgainst",
+  1: "dateString",
+  2: "time",
+  3: "court",
+  6: "teamAgainst",
 };
 
 export function getThisWeeksGameFromHtml(html: any) {
-  const $ = cheerio.load(html);
-
-  const table = $("tbody");
-
-  // Initialize an empty array to store the table data
-  const tableData: any = [];
-
-  // Iterate over each row of the table using the find and each methods
-  table.find("tr").each((_i, row) => {
-    // Initialize an empty object to store the row data
-    const rowData: any = {};
-
-    // Iterate over each cell of the row using the find and each methods
-    $(row)
-      .find("td, th")
-      .each((j, cell) => {
-        // Add the cell data to the row data object
-        rowData[$(cell).text()] = j;
-      });
-
-    // Add the row data to the table data array
-    tableData.push(rowData);
-  });
+  const games = getDataStructureFromHTML(html);
 
   const oneWeekFromNow = moment().add(6, "days");
   const now = moment();
 
-  // Remove the top two rows, they are junk
-  tableData.splice(0, 2);
-  const cleaned = tableData.map((row: any) => {
-    const keys = Object.keys(row);
-    const time = keys[2];
-    const date = keys[1].split(" ")[0];
-    return {
-      gameInfo: `${moment(date, "DD-MM-YY").format("dddd")} at ${moment(
-        time,
-        "HH:mm"
-      ).format("ha")} vs. ${keys[5]} on ${keys[3]}`,
-      date,
-    };
-  });
-
-  const correct = cleaned.find((item: any) => {
-    const gameDate = moment(item.date, "DD-MM-YY");
+  const thisWeeksGame = games.find((game: Game) => {
+    const gameDate = moment(game.dateString, "DD-MM-YY");
     return gameDate.isBetween(now, oneWeekFromNow);
   });
-  return correct;
+
+  return {
+    ...thisWeeksGame,
+    gameInfo: `${moment(thisWeeksGame.dateString, "DD-MM-YY").format(
+      "dddd"
+    )} at ${moment(thisWeeksGame.time, "HH:mm").format("ha")} vs. ${
+      thisWeeksGame.teamAgainst
+    } on ${thisWeeksGame.court}`,
+  };
 }
 
 export function getDataStructureFromHTML(html: string) {
@@ -64,7 +36,7 @@ export function getDataStructureFromHTML(html: string) {
   const table = $("tbody");
 
   // Initialize an empty array to store the table data
-  const tableData: any = [];
+  const tableData: Array<Game> = [];
 
   // Iterate over each row of the table using the find and each methods
   table.find("tr").each((index, row) => {
@@ -89,5 +61,10 @@ export function getDataStructureFromHTML(html: string) {
     tableData.push(rowData);
   });
 
-  return tableData;
+  return tableData.map((row: any) => {
+    return {
+      ...row,
+      dateString: row.dateString.split(" ")[0],
+    };
+  });
 }
--- src/main.GetGamesFunction.test.ts
+++ src/main.GetGamesFunction.test.ts
@@ -3,30 +3,35 @@ import {
   getThisWeeksGameFromHtml,
   getDataStructureFromHTML,
 } from "./lib/getThisWeeksGameFromHtml";
+import { Game } from "./types";
 
 const html = readFileSync("./src/sample-table.html", "utf-8");
 
 test("Test the extract of games from HTML function", () => {
-  const expected = {
-    date: "16/05/23",
-    gameInfo: "Tuesday at 9pm vs. Shesqueal O'Neal on Court 3 ",
+  const expected: Game = {
+    gameInfo: "Tuesday at 9pm vs. Shesqueal O'Neal on Court 3",
+    court: "Court 3",
+    teamAgainst: "Shesqueal O'Neal",
+    time: "21:00",
+    dateString: "16/05/23",
   };
   const result = getThisWeeksGameFromHtml(html);
-  return expect(result).toEqual(expected);
+  return expect(result).toStrictEqual(expected);
 });
 
-test("Test the extract of games has the correct properties", () => {
+test("Test the extract of games has the correct game info", () => {
   const result = getThisWeeksGameFromHtml(html);
-  expect(result).toHaveProperty("date");
-  expect(result).toHaveProperty("gameInfo");
+  expect(result.gameInfo).toEqual(
+    "Tuesday at 9pm vs. Shesqueal O'Neal on Court 3"
+  );
 });
 
 test("The data structure returned from the HTML is useful", () => {
   const expected = {
-    Date: "02/05/23 (Tue)",
-    TeamAgainst: "G-Bangers",
-    Court: "Court 2",
-    Time: "19:00",
+    dateString: "02/05/23",
+    teamAgainst: "G-Bangers",
+    court: "Court 2",
+    time: "19:00",
   };
   const actual = getDataStructureFromHTML(html)[0];
   expect(actual).toEqual(expected);
